cd7f89027e1d76b0a95b0fc5c08277494f8c4095
milosssss9 <milosz.borczynski12@gmail.com>
2022-11-03 13:27:43 +0100

    policz02

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int policz(int a, int b){
-    return a+b;
+    return a-b;
 }
 int main() {
-    printf("pierwszy branch 2+3=%d", policz(2,3));
+    printf("drugi branch 2-3=%d", policz(2,3));
     return 0;
 }
